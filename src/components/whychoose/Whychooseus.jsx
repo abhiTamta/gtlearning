54af143b8520225c511a96db074409f3dd532430
abhiTamta <abhinay.tamta@hotmail.com>
2024-10-29 00:44:50 +0530

    commitl

--- src/components/whychoose/Whychooseus.jsx
+++ src/components/whychoose/Whychooseus.jsx
@@ -1,33 +1,69 @@
 import { Card, Col, Row } from 'antd'
-import React from 'react'
+import icon1 from '../../assets/images/graduation.png'
 
 export const Whychooseus = () => {
   return (
-    <div className='w-full bg-[#502c7d] py-12'>
+    <div className='w-full bg-[url("../../assets/images/map_bg.png")] bg-slate-50 py-12'>
         <div className="container mx-auto">
             <div className="text-center mb-10">
-                <h2 className='font-bold text-white text-4xl'>Why choose us?</h2>
+                <h2 className='font-bold text-4xl'>Why choose us?</h2>
             </div>
             <Row className='mb-8' gutter={30} justify={'center'}>
                 <Col span={6}>
-                    <Card>Whychooseus</Card>
+                    <Card className='bg-purple-100'>
+                        <div className="cardIcon w-20 mx-auto mb-5">
+                            <img src={icon1} alt="" />
+                        </div>
+                        <div className="cardTitle font-bold text-center text-xl mb-3">Lorem, ipsum dolor.</div>
+                        <div className="cardDiscription text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur doloribus architecto eius, dicta ut, vel quasi adipisci eligendi, ex qui totam possimus!</div>
+                    </Card>
                 </Col>
                 <Col span={6}>
-                    <Card>Whychooseus</Card>
+                    <Card className='bg-purple-100'>
+                        <div className="cardIcon w-20 mx-auto mb-5">
+                            <img src={icon1} alt="" />
+                        </div>
+                        <div className="cardTitle font-bold text-center text-xl mb-3">Lorem, ipsum dolor.</div>
+                        <div className="cardDiscription text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur doloribus architecto eius, dicta ut, vel quasi adipisci eligendi, ex qui totam possimus!</div>
+                    </Card>
                 </Col>
                 <Col span={6}>
-                    <Card>Whychooseus</Card>
+                    <Card className='bg-purple-100'>
+                        <div className="cardIcon w-20 mx-auto mb-5">
+                            <img src={icon1} alt="" />
+                        </div>
+                        <div className="cardTitle font-bold text-center text-xl mb-3">Lorem, ipsum dolor.</div>
+                        <div className="cardDiscription text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur doloribus architecto eius, dicta ut, vel quasi adipisci eligendi, ex qui totam possimus!</div>
+                    </Card>
                 </Col>                
             </Row>
             <Row className='' gutter={30} justify={'center'}>
                 <Col span={6}>
-                    <Card>Whychooseus</Card>
+                    <Card className='bg-purple-100'>
+                        <div className="cardIcon w-20 mx-auto mb-5">
+                            <img src={icon1} alt="" />
+                        </div>
+                        <div className="cardTitle font-bold text-center text-xl mb-3">Lorem, ipsum dolor.</div>
+                        <div className="cardDiscription text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur doloribus architecto eius, dicta ut, vel quasi adipisci eligendi, ex qui totam possimus!</div>
+                    </Card>
                 </Col>
                 <Col span={6}>
-                    <Card>Whychooseus</Card>
+                    <Card className='bg-purple-100'>
+                        <div className="cardIcon w-20 mx-auto mb-5">
+                            <img src={icon1} alt="" />
+                        </div>
+                        <div className="cardTitle font-bold text-center text-xl mb-3">Lorem, ipsum dolor.</div>
+                        <div className="cardDiscription text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur doloribus architecto eius, dicta ut, vel quasi adipisci eligendi, ex qui totam possimus!</div>
+                    </Card>
                 </Col>
                 <Col span={6}>
-                    <Card>Whychooseus</Card>
+                    <Card className='bg-purple-100'>
+                        <div className="cardIcon w-20 mx-auto mb-5">
+                            <img src={icon1} alt="" />
+                        </div>
+                        <div className="cardTitle font-bold text-center text-xl mb-3">Lorem, ipsum dolor.</div>
+                        <div className="cardDiscription text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur doloribus architecto eius, dicta ut, vel quasi adipisci eligendi, ex qui totam possimus!</div>
+                    </Card>
                 </Col>                
             </Row>
         </div>
